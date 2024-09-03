fb1613d263581b779a35f424452ae949787fc038
rumbleFTW <007rajdeepghosh@gmail.com>
2024-09-03 23:58:09 +0530

    sync nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,11 @@
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
+  { "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- load colorscheme here
+      vim.cmd([[colorscheme gruvbox]])
+    end
+  }
 }
