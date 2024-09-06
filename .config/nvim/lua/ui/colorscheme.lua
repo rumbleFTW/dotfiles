c16a16c4e5f875ef2ee7f25a3db35c8ca39c2930
rumbleFTW <007rajdeepghosh@gmail.com>
2024-09-06 18:40:43 +0530

    monochrome

--- .config/nvim/lua/ui/colorscheme.lua
+++ .config/nvim/lua/ui/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim",
+  { "kdheepak/monochrome.nvim",
     lazy = false,
     priority = 1000,
     config = function()
       -- load colorscheme here
-      vim.cmd([[colorscheme gruvbox]])
+      vim.cmd([[colorscheme monochrome]])
     end
   }
 }
